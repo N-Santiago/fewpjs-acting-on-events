4a92e71fa3402779434156b39479b5cf159f6a32
N-Santiago <norbertosantiago82@gmail.com>
2021-02-09 01:07:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px"; 
+
+const leftNumbers = dodger.style.left.replace("px", "");
+
+function moveDodgerLeft() {
+    leftNumbers
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight() {
+    leftNumbers
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+   });
